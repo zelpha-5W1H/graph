d15b9f45d4486f4954ef3999ebc5e255b8932388
zelpha-5W1H <tharunkothari@yahoo.com>
2023-05-08 07:53:14 +0530

    Fixed __init__.py

---
+++ build/lib/graph_z/__init__.py
@@ -0,0 +1,4 @@
+from .graph_z import (
+    Vertex,
+    Graph
+)
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 setup(
     name="graph_z_c",
-    version="0.0.2",
+    version="0.0.3",
     description="Structures to play with graphs",
     packages=['graph_z']    
 )
