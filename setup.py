251e5946a40c2a997eced8d986acd6501be43b37
zelpha-5W1H <tharunkothari@yahoo.com>
2023-05-08 07:46:56 +0530

    Version 2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 setup(
     name="graph_z_c",
-    version="0.0.1",
+    version="0.0.2",
     description="Structures to play with graphs",
     packages=['graph_z']    
 )
